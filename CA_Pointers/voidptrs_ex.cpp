9dcd994aeb7a1c0606955b0422461b07ec2dda5d
4MANN444 <nickross4444@gmail.com>
2021-02-19 17:05:44 -0700

    formatting

--- CA_Pointers/voidptrs_ex.cpp
+++ CA_Pointers/voidptrs_ex.cpp
@@ -4,11 +4,15 @@ void increase (void* data, int psize)
 {
 	if ( psize == sizeof(char) )
 	{
-	       	char* pchar; pchar=(char*)data; ++(*pchar);
+	       	char* pchar; 
+		pchar=(char*)data; 
+		++(*pchar);
        	}
 	else if (psize == sizeof(int) )
 	{
-	       	int* pint; pint=(int*)data; ++(*pint); 
+	       	int* pint; 
+		pint=(int*)data; 
+		++(*pint); 
 	}
 }
 int main ()
